1b2b126e8fa1452529ff7093c9a72de68c438b1f
Hubert Karbowy <hk@hubertkarbowy.pl>
2021-04-13 15:55:45 +0200

    LM demo with variable input length (raggedtensors)

--- lstm_with_wordpieces/04_demo.py
+++ lstm_with_wordpieces/04_demo.py
@@ -56,29 +56,38 @@ def prepare_sequences(spmproc, args):
     return x_sequences
 
 def predict_next_n_pieces(model, spmproc, sent, args):
-    encoded = spmproc.encode_as_pieces(sent)
+    piece_ids = spmproc.encode_as_ids(sent)
     cnt = 0
     for i in range(args['max_lookahead_tokens']):
-        print(f"Encoded as {len(encoded)} pieces: {encoded}")
-        last_token_idx = len(encoded) - 1 if args['padding_direction'] == 'post' \
-                         else args['max_seq_len'] - 1
-        x_hat = tf.keras.preprocessing.sequence.pad_sequences(
-                   np.array([spmproc.encode_as_ids(spmproc.decode_pieces(encoded))]), \
-                   maxlen=args['max_seq_len'],
-                   value=PAD_ID,
-                   padding=args['padding_direction']
-                   )
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        print(piece_ids)
+        print(f"Encoded as {len(piece_ids)} pieces: {[spmproc.id_to_piece(piece) for piece in piece_ids]}")
+        if args.get('max_seq_len') is not None: # model saved with a fixed sequence length, so adding padding
+            last_token_idx = len(encoded) - 1 if args['padding_direction'] == 'post' \
+                else args['max_seq_len'] - 1
+            x_hat = tf.keras.preprocessing.sequence.pad_sequences(
+                np.array([piece_ids]), \
+                maxlen=args['max_seq_len'],
+                value=PAD_ID,
+                padding=args['padding_direction']
+            )
+        else:
+            x_hat = tf.ragged.constant([piece_ids])
+            last_token_idx = len(piece_ids) - 1
         y_hat = model.predict(x_hat)
+
         next_k_piece_ids = heapq.nlargest(args['beam_width'], \
                                           range(0, spmproc.vocab_size()), \
-                                          y_hat[0, last_token_idx].take)
-        next_k_piece_probs = y_hat[0, last_token_idx, next_k_piece_ids]
+                                          np.array(y_hat[0, last_token_idx]).take)
+        print(next_k_piece_ids)
+        next_k_piece_probs = np.array(y_hat[0, last_token_idx]).take([next_k_piece_ids]).tolist()[0]
+        print(next_k_piece_probs)
         next_k_pieces = [spmproc.id_to_piece(p) for p in next_k_piece_ids]
         print(f"Candidate next pieces: {list(zip(next_k_pieces, next_k_piece_probs))}")
         cnt += 1
-        if next_k_piece_ids == EOS_ID or cnt >= args['max_lookahead_tokens']:
+        if next_k_piece_ids[0] == EOS_ID or cnt >= args['max_lookahead_tokens']:
             break
-        encoded.append(next_k_pieces[0])
+        piece_ids.append(next_k_piece_ids[0])
 
 def main(args):
     spmproc = spm.SentencePieceProcessor(args['spm_model_file'])
@@ -99,8 +108,8 @@ if __name__ == "__main__":
     parser.add_argument("--spm-model-file", required=True, help="SPM .model file")
     parser.add_argument("--add-bos", required=False, action='store_true', help="Will add <s> tokens")
     parser.add_argument("--add-eos", required=False, action='store_true', help="Will add </s> tokens. Should generally not be added for prediction/demo")
-    parser.add_argument("--max-seq-len", required=True, type=int, help="Maximum number of wordpiece tokens in a sequence")
-    parser.add_argument("--padding-direction", choices=['pre', 'post'], default='post', help="Pre or post padding (for LM training 'post' seems better than 'pre')")
+    parser.add_argument("--max-seq-len", required=False, type=int, help="Maximum number of wordpiece tokens in a sequence. If not given, will assume input is a ragged tensor.")
+    parser.add_argument("--padding-direction", choices=['pre', 'post'], default='post', help="Pre or post padding (for LM training 'post' seems better than 'pre'). Irrelevant if input is a ragged tensor.")
     parser.add_argument("--beam-width", type=int, default=4, help="Beam search not implemented, defaulting to 1 (greedy search)")
     parser.add_argument("--max-lookahead-tokens", type=int, default=3, help="Maximum number of tokens to generate after input sequence. The 'decoder' will stop when it hits </s>.")
     argz = parser.parse_args()
