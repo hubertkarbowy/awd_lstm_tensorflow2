72423de57ab75b8774efd66159aab71ce2ec0bcd
Hubert Karbowy <hk@hubertkarbowy.pl>
2021-04-19 13:28:50 +0200

    Minor fix to ULMFiT sequence tagger

--- almost_ulmfit_seq_tagging.py
+++ almost_ulmfit_seq_tagging.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 from .almost_ulmfit_tf2 import almost_ulmfit_model
 
-
-def ulmfit_quick_and_dirty_sequence_tagger(*, num_classes=3, pretrained_weights='wagi'):
+def ulmfit_quick_and_dirty_sequence_tagger(*, num_classes=3, pretrained_weights=None):
     print("Building model from Python code (not tf.saved_model)...")
     au = almost_ulmfit_model()
-    print("Restoring weights from file....")
-    au.load_weights(pretrained_weights)
+    if pretrained_weights is not None:
+        print("Restoring weights from file....")
+        au.load_weights(pretrained_weights)
+    else:
+        print("!!! THE MODEL WEIGHTS ARE UNINITIALIZED !!! Make sure to restore them from file.")
     print("Removing LM head")
     au.pop() # remove final dropout
     au.pop() # remove LM head
