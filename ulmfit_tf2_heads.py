651172f247a708a27ec2a0bd9b00fb20620aa8a3
Hubert Karbowy <hk@hubertkarbowy.pl>
2021-04-26 14:29:34 +0200

    Add fake (plain LSTM) tagger

--- ulmfit_tf2_heads.py
+++ ulmfit_tf2_heads.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 from .ulmfit_tf2 import tf2_ulmfit_encoder
 
-def ulmfit_sequence_tagger(*, num_classes=3, pretrained_weights=None, spm_model_file,
+def ulmfit_sequence_tagger(*, num_classes=3, pretrained_weights=None, fixed_seq_len=None, spm_model_file,
                               also_return_spm_encoder=False):
     print("Building model from Python code (not tf.saved_model)...")
-    _, enc_num, _, spm_encoder_model = tf2_ulmfit_encoder(fixed_seq_len=None, spm_model_file=spm_model_file)
+    _, enc_num, _, spm_encoder_model = tf2_ulmfit_encoder(fixed_seq_len=fixed_seq_len, spm_model_file=spm_model_file)
     if pretrained_weights is not None:
         print("Restoring weights from file....")
         enc_num.load_weights(pretrained_weights)
@@ -17,3 +17,22 @@ def ulmfit_sequence_tagger(*, num_classes=3, pretrained_weights=None, spm_model_
         return tagger, spm_encoder_model
     else:
         return tagger
+
+def ulmfit_fake_tagger(*, num_classes=3, pretrained_weights=None, fixed_seq_len=None, spm_model_file,
+                          also_return_spm_encoder=False):
+    print("Building a regular LSTM model using only standard Keras blocks...")
+    fake_model = tf.keras.models.Sequential([
+        tf.keras.layers.Input((fixed_seq_len,)),
+        tf.keras.layers.Masking(mask_value=1),
+        tf.keras.layers.Embedding(35000, 400),
+        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.SpatialDropout1D(0.3),
+        tf.keras.layers.LSTM(1152, return_sequences=True),
+        tf.keras.layers.SpatialDropout1D(0.5),
+        tf.keras.layers.LSTM(1152, return_sequences=True),
+        tf.keras.layers.SpatialDropout1D(0.5),
+        tf.keras.layers.LSTM(400, return_sequences=True),
+        tf.keras.layers.SpatialDropout1D(0.5),
+        tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(3, activation='softmax'))
+        ])
+    return fake_model
